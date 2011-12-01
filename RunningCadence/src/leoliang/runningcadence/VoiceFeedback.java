0ece369b92b917a4c3ea79401951496fa019d6f4
Leo Liang <leoliang@gmail.com>
2011-12-02 00:58:18 +0800

    Improve voice feedback, less feedback when status jumping between on target and out of target cadence.

--- RunningCadence/src/leoliang/runningcadence/VoiceFeedback.java
+++ RunningCadence/src/leoliang/runningcadence/VoiceFeedback.java
@@ -5,14 +5,15 @@ import android.util.Log;
 public class VoiceFeedback {
 
 	private enum RunningState {
-		NOT_RUNNING, RUNNING_ON_TARGET_CADENCE, RUNNING_OUT_OF_TARGET_CADENCE
+		NOT_RUNNING, RUNNING_ON_TARGET_CADENCE, RUNNING_SLOWER_THAN_TARGET_CADENCE, RUNNING_FASTER_THAN_TARGET_CADENCE
 	}
 
 	/**
 	 * Voice feedback interval, from last state to current state.
 	 * feedbackInterval[lastFeedbackState.ordinal()][currentState.ordinal()]
 	 */
-	private final static int feedbackInterval[][] = { { 60, 10, 10 }, { 5, 180, 5 }, { 5, 10, 30 } };
+	private final static int feedbackInterval[][] = { { 60, 10, 10, 10 }, { 5, 180, 5, 5 }, { 5, 10, 30, 30 },
+			{ 5, 10, 30, 30 } };
 
 	private final static String TAG = "VoiceFeedback";
 
@@ -43,10 +44,33 @@ public class VoiceFeedback {
 		RunningState currentState;
 		if (cadence <= 0) {
 			currentState = RunningState.NOT_RUNNING;
-		} else if (Math.abs(cadence - targetCadence) <= 2) {
-			currentState = RunningState.RUNNING_ON_TARGET_CADENCE;
 		} else {
-			currentState = RunningState.RUNNING_OUT_OF_TARGET_CADENCE;
+			int allowedDeviation = (int) (targetCadence * 0.025);
+			int slowThresold = 0;
+			int fastThresold = 0;
+			switch (lastFeedbackState) {
+			case NOT_RUNNING:
+			case RUNNING_ON_TARGET_CADENCE:
+				slowThresold = targetCadence - allowedDeviation;
+				fastThresold = targetCadence + allowedDeviation;
+				break;
+			case RUNNING_FASTER_THAN_TARGET_CADENCE:
+				slowThresold = targetCadence - allowedDeviation;
+				fastThresold = targetCadence;
+				break;
+			case RUNNING_SLOWER_THAN_TARGET_CADENCE:
+				slowThresold = targetCadence;
+				fastThresold = targetCadence + allowedDeviation;
+				break;
+			}
+
+			if (currentCadence > fastThresold) {
+				currentState = RunningState.RUNNING_FASTER_THAN_TARGET_CADENCE;
+			} else if (currentCadence < slowThresold) {
+				currentState = RunningState.RUNNING_SLOWER_THAN_TARGET_CADENCE;
+			} else {
+				currentState = RunningState.RUNNING_ON_TARGET_CADENCE;
+			}
 		}
 
 		int interval = feedbackInterval[lastFeedbackState.ordinal()][currentState.ordinal()];
@@ -64,13 +88,13 @@ public class VoiceFeedback {
 		case RUNNING_ON_TARGET_CADENCE:
 			ttsOutput.say("Good job! You're running on target cadence. Keep going.");
 			break;
-		case RUNNING_OUT_OF_TARGET_CADENCE:
+		case RUNNING_FASTER_THAN_TARGET_CADENCE:
 			ttsOutput.say(String.format("Current cadence is %d cycles per minute.", currentCadence));
-			if (currentCadence > targetCadence) {
-				ttsOutput.say("Slow down.");
-			} else {
-				ttsOutput.say("Speed up.");
-			}
+			ttsOutput.say("Slow down.");
+			break;
+		case RUNNING_SLOWER_THAN_TARGET_CADENCE:
+			ttsOutput.say(String.format("Current cadence is %d cycles per minute.", currentCadence));
+			ttsOutput.say("Speed up.");
 			break;
 		}
 		lastFeedbackState = currentState;
